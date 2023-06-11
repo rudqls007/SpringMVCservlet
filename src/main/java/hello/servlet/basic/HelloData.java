6c78812c05d3f95b5c04da3f64e5ea02920ee0e7
rudqls007 <rudqls007@gmail.com>
2023-06-11 17:00:22 +0900

    HTTP 요청 데이터 - API 메세지 바디 - JSON

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,16 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+
+
+
+
+
+}
