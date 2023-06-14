6b63911aa0b122fb1765e7032364228ca17202d6
rudqls007 <rudqls007@gmail.com>
2023-06-14 21:21:40 +0900

    회원 관리 웹 애플리케이션 요구사항

---
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,57 @@
+package hello.servlet.domain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class MemberRepositoryTest {
+
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    // 테스트 하나씩 끝날 때마다 초기화 시켜줌.
+    @AfterEach
+    void afterEach(){
+        memberRepository.clearStore();
+    }
+
+
+    @Test
+    void save(){
+        //given
+        Member member = new Member("hello", 20);
+        //when
+        Member savedMember = memberRepository.save(member);
+
+
+        //then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+
+    @Test
+    void findAll(){
+
+        //given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        //when
+        List<Member> result = memberRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+        // result 안에 멤버1, 멤버2 객체가 있냐? = contains
+        assertThat(result).contains(member1, member2);
+    }
+
+}
