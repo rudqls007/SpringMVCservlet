ee10f71a6a9038fda46df5700bd9b93fd42159c7
rudqls007 <rudqls007@gmail.com>
2023-06-19 23:16:17 +0900

    스프링 MVC

--- src/main/java/hello/servlet/web/frontcontroller/v5/ControllerV4HandlerApdater.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/ControllerV4HandlerApdater.java
@@ -22,7 +22,7 @@ public class ControllerV4HandlerApdater implements MyHandlerAdapter {
     public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
         ControllerV4 controller = (ControllerV4) handler;
 
-        Map<String, String> paramMap = createParamMap(request);
+        Map<String, String> paramMap = createParamMap(request)  ;
         HashMap<String, Object> model = new HashMap<>();
 
         String viewName = controller.process(paramMap, model);
---
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,17 @@
+package hello.servlet.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+// 스프링 빈 이름  URL 패턴으로 맞춤.
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return new ModelAndView("new-form");
+    }
+}
